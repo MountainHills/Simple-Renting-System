7786d52975b8c8d0ac2d4011713a8aecd2459850
Anton Bondoc <antonbondoc14@gmail.com>
2021-06-10 22:34:33 +0800

    Entering the Rent System

---
+++ src/RentApp.java
@@ -0,0 +1,14 @@
+import GUI.LoginGUI;
+
+/*
+    This java file is responsible for initializing the Renting System.
+*/
+
+public class RentApp {
+    public static void main(String[] args) 
+    {
+        System.out.println("This is for the world at stake.");
+        
+        LoginGUI gui = new LoginGUI();
+    }
+}
